c4f08b29fbf50f8a7f7e17d821251bd069e4e4f6
denure <daina.emmanuel@aol.com>
2015-02-19 19:59:04 +0530

    ## First Draft of Cleaning Data Assignment
    ## Achieved question 1-4

---
+++ README.md
@@ -0,0 +1,77 @@
+---
+title: "Getting and Cleaning Data Course Project"
+author: "Daina Emmanuel"
+date: "`r Sys.Date()`"
+output: rmarkdown::html_vignette
+GCD Course Project: >
+  %\VignetteIndexEntry{Vignette Title}
+  %\VignetteEngine{knitr::rmarkdown}
+  \usepackage[utf8]{inputenc}
+---
+
+##Reading all data 
+
+Input Available : 
+Observations per person, activity and test type ie either training or test 
+
+To Tidy the data :
+What we need to do : 
+1. 
+2. 
+3. 
+4. 
+
+
+
+
+
+## Assignement Question : 
+You should create one R script called run_analysis.R that does the following. 
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names. 
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+## R Script -  Info
+
+Note the various macros within the `vignette` setion of the metadata block above. These are required in order to instruct R how to build the vignette. Note that you should change the `title` field and the `\VignetteIndexEntry` to match the title of your vignette.
+
+## Code Book
+
+The `html_vignette` template includes a basic CSS theme. To override this theme you can specify your own CSS in the document metadata as follows:
+
+    output: 
+      rmarkdown::html_vignette:
+        css: mystyles.css
+
+## Data Set
+
+The figure sizes have been customised so that you can easily put two images side-by-side. 
+
+```{r, fig.show='hold'}
+plot(1:10)
+plot(10:1)
+```
+
+You can enable figure captions by `fig_caption: yes` in YAML:
+
+    output:
+      rmarkdown::html_vignette:
+        fig_caption: yes
+
+Then you can use the chunk option `fig.cap = "Your figure caption."` in **knitr**.
+
+## More Examples
+
+You can write math expressions, e.g. $Y = X\beta + \epsilon$, footnotes^[A footnote here.], and tables, e.g. using `knitr::kable()`.
+
+```{r, echo=FALSE, results='asis'}
+knitr::kable(head(mtcars, 10))
+```
+
+Also a quote using `>`:
+
+> "He who gives up [code] safety for [code] speed deserves neither."
+([via](https://twitter.com/hadleywickham/status/504368538874703872))
---
+++ run_analysis.R
@@ -0,0 +1,104 @@
+library(reshape2)
+library(Hmisc)
+library(dplyr)
+
+## Script to analyse the wearable computing results 
+## Author : Daina Emmanuel  
+
+## Set the working directory where the observation files are available
+setwd("C://Personal//Coursera//CleaningData//GCD Course Project//getdata-projectfiles-UCI HAR Dataset//UCI HAR Dataset")
+
+## Reading the relevant files as table 
+## features.txt <- this will be an input for the variables names 
+## The observation are of 2 category : 1 recorded for training and the other for test.
+## subject_train/subject_test <- input for the subject corresponding to whom the observation is collected
+## X_train/X_test <- the measures of the observed variables 
+## Y_train/Y_test <- value corresponding to the activity for which the measures are recorded
+
+feature <- read.table("features.txt",stringsAsFactors=FALSE,colClasses = 'character')
+subject_train <- read.table("train//subject_train.txt",stringsAsFactors=FALSE,colClasses = 'character')
+X_train <- read.table("train//X_train.txt",stringsAsFactors=FALSE)
+Y_train <- read.table("train//y_train.txt",stringsAsFactors=FALSE,colClasses = 'character')
+subject_test <- read.table("test//subject_test.txt",stringsAsFactors=FALSE,colClasses = 'character')
+X_test <- read.table("test//X_test.txt",stringsAsFactors=FALSE,colClasses = 'character')
+Y_test <- read.table("test//y_test.txt",stringsAsFactors=FALSE,colClasses = 'character')
+
+##
+# Appropriately labels the data set with descriptive variable names. 
+
+nameCol <- c((make.names(feature$V2, unique=TRUE, allow_ = TRUE)))
+names(X_train) <- nameCol
+
+## preserving the measure names from the features file , 
+## this will help in easier interpretation of data
+
+names(Y_train)<- "obsActivity"
+names(subject_train) <- "obsID"
+names(X_test) <- nameCol
+names(Y_test)<- "obsActivity"
+names(subject_test) <- "obsID"
+
+## generating training data 
+train_ind_data <- cbind(subject_train
+                        ,obsType="train"
+                        ,Y_train
+                        ,X_train
+
+)
+# generating test data 
+test_ind_data <- cbind(subject_test
+                       ,obsType="test"
+                        ,Y_test
+                        ,X_test
+)
+# Merges the training and the test sets to create one data set.
+
+observation_table1 <- rbind(train_ind_data,test_ind_data)
+print(dim(observation_table1))
+
+
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
+## subset by mean and std and bind the same
+mean_subset <-select(observation_table1, contains("mean"))
+std_subset <- select(observation_table1, contains("std"))
+
+observation_table2 <- cbind(observation_table1$obsID,
+                (observation_table1$obsType),
+                observation_table1$obsActivity,
+                mean_subset,
+                std_subset)
+
+names(observation_table2)[1] <-"obsID"
+names(observation_table2)[2] <-"obsType"
+names(observation_table2)[3] <-"obsActivity"
+observation_table2 <- tbl_df(observation_table2)
+
+# Uses descriptive activity names to name the activities in the data set
+
+observation_table2$obsActivity_str <- "TBD"
+observation_table2$obsActivity_str[which(as.character(observation_table2$obsActivity) =="1")] <- "WALKING"
+observation_table2$obsActivity_str[which(as.character(observation_table2$obsActivity) =="2")] <- "WALKING_UPSTAIRS"
+observation_table2$obsActivity_str[which(as.character(observation_table2$obsActivity) =="3")] <- "WALKING_DOWNSTAIRS"
+observation_table2$obsActivity_str[which(as.character(observation_table2$obsActivity) =="4")] <- "SITTING"
+observation_table2$obsActivity_str[which(as.character(observation_table2$obsActivity) =="5")] <- "STANDING"
+observation_table2$obsActivity_str[which(as.character(observation_table2$obsActivity) =="6")] <- "LAYING"
+
+
+# From the data set in step 4, creates a second, independent tidy data set
+# with the average of each variable for each activity and each subject.
+
+
+observation_table3 <- group_by(observation_table2,obsID,obsActivity)
+id <- c(4:89) 
+observation_table3[,id] <- as.numeric(as.character(unlist(observation_table3[,id]),
+                                       drop=FALSE,recursive =FALSE,
+                                       use.names=FALSE))
+
+observation_table3 <- observation_table3[,c(2,90,1,3,4:89)]
+View(observation_table3)
+
+#Now I need to find the summarise_each by groups which is becoming very damn confusing . 
+#by_table3 <- summarise(by_table3, average = ave(average))
+
+#by_table3<- summarise_each(table4, funs(ave))
+
